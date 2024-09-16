0cd1831e0159dc246df8c06446e85ef46738654a
jerrymckeejr <jmckee0882@student.fullsail.edu>
2024-09-16 14:47:21 -0400

    updating the hello_swagger program

--- hello_swagger/main.go
+++ hello_swagger/main.go
@@ -1,9 +1,41 @@
 package main
 
 import (
+	
 	"github.com/gin-gonic/gin"
-	ginSwagger "github.com/swaggo/gin-swagger"
-	swaggerFiles "github.com/swaggo/files"
-	docs "github.com/go-project-name/docs"
+	docs "example/hello_swagger/docs"
+	ginSwagger "github.com/swaggo/files"
+	swaggerFiles "github.com/swaggo/gin-swagger"
+
+	
 	"net/http"
 )
+
+// @BasePath /api/v1
+
+// PingExample godoc
+// @Summary ping example
+// @Schemes
+// @Description do ping
+// @Tags example
+// @Accept json
+// @Produce json
+// @Success 200 {string} HelloWorld
+// @Router /example/helloworld [get]
+func HelloWorld(g *gin.Context){
+	g.JSON(http.StatusOK, "helloworld")
+}
+
+func main() {
+	r := gin.Default()
+	docs.SwaggerInfo.BasePath = "/api/v1"
+	v1 := r.Group("/api/v1")
+	{
+		eg := v1.Group("/example")
+		{
+			eg.GET("/helloworld", HelloWorld)
+		}
+	}
+	r.GET("/swagger/*any", ginSwagger.WrapHandler())
+	r.Run(":8080")
+}

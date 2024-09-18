5c5153081311f852f851a56f784b906b74936f0d
Jerry McKee Jr <jmckeejr@safehomesecurityinc.com>
2024-09-18 09:31:47 -0400

    building out the base API

--- web-service-mux/main.go
+++ web-service-mux/main.go
@@ -1,18 +1,38 @@
 package main
 
 import(
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
-
 	"github.com/gorilla/mux"
 )
+
+// Defining a Book struct
+type Book struct {
+	ID 			string		`json:"id,omitempty"`
+	Title 		string		`json:"title,omitempty"`
+	Author 		string		`json:"author,omitempty"`
+	Publisher 	*Company	`json:"publisher,omitempty"`
+}
+// Definign a Company struct
+type Company struct{
+	Name 		string		`json:"name,omitempty"`
+	Address 	string		`json:"address,omitempty"`
+}
+
+var books []Book
+
+func GetBooks(w http.ResponseWriter, r *http.Request){
+	json.NewEncoder(w).Encode(books)
+}
 
 func main() {
 	router := mux.NewRouter()
 
-	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
-		fmt.Fprint(w, "Hello World!")
-	})
+	router.HandleFunc("/books", GetBooks).Methods("GET")
+	
 
-	http.ListenAndServe(":8080", router)
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
+
